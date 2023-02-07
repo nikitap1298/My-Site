7619d415ba149b47aedd682acef7a807a2862a9b
Nikita <nikita.pishchugin@gmail.com>
2023-02-07 17:14:13 +0100

    Small changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
       <div className="header-div">
         <img class="brandPic" src={brandPic} alt="Brand-Pic" />
         <div>
-          <a className="header-a" href="https://www.apple.com">About</a>
+          <a className="header-a" href="about.html">About</a>
           <a className="header-a" href="https://www.apple.com">Projects</a>
           <a className="header-a" href="https://www.apple.com">Photos</a>
           <a className="header-a" href="https://www.apple.com">Contact</a>
