c0e86c1be042aa633460afb7236cd1a5456e45eb
Nikita <nikita.pishchugin@gmail.com>
2023-02-08 16:39:42 +0100

    Finish About page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,14 @@
 import React from "react"
 import Header from "../reusable/Header"
+import AboutMeContainer from "./AboutMeContainer"
+import Footer from "../reusable/Footer"
 
 function About() {
   return (
-    <div className="app-jsx">
+    <div className="app-jsx" style={{height: "100vh"}}>
       <Header />
+      <AboutMeContainer />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/about/AboutMeContainer.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+function AboutMeContainer() {
+  return (
+    <div className="about-me-container">
+      <h1 className="about-me-h1" >About Me</h1>
+      <p className="about-me-p">
+        I am an Web & iOS Developer who just started my journey in tech. I am a
+        career changer driven by curiosity and believe software development
+        maximises my potential like no other field. I am very eager to apply my
+        skills and master new ones in both a supportive and challenging
+        environment that makes me grow professionally and personally.
+      </p>
+    </div>
+  )
+}
+
+export default AboutMeContainer
